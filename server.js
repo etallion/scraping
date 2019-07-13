69122420cf37dd9cf96c86004ed1046add893799
Will Mangimelli <willwinman@gmail.com>
2019-07-13 10:37:01 -0500

    remove space

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 mongoose.connect(MONGODB_URI);
+
 // Routes
 
 // A GET route for scraping the echoJS website
